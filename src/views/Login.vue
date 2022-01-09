041084d92c8046728be7b3096011f3d48f0baf8b
Helton Carlos <heltoncarlospa@hotmail.com>
2022-01-08 21:14:35 -0300

    salvar

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div style="background-color: #ccc">
+      <form>
+        <label>Login</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="password" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
