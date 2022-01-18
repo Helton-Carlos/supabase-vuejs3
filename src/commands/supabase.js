1fafab22f93fd1c90dc84d6ff12c4b8445d6d5c3
Helton Carlos <heltoncarlospa@hotmail.com>
2022-01-17 22:25:29 -0300

    mudando API

--- src/commands/supabase.js
+++ src/commands/supabase.js
@@ -1,9 +1,9 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl="https://kdsyenoxljzipllzsder.supabase.co"
-const supabaseKey="e64b9f04-902c-469c-82be-993324adcc08"
+const supabaseUrl="https://qyvzyshzxcutgozjsoxz.supabase.co"
+const supabaseKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyMzgyMTk1LCJleHAiOjE5NTc5NTgxOTV9.tvrNxqMu_cAmkPcwaXL8ZXwl_5MUPonp_m5thKKM03U"
 const supabase=createClient(supabaseUrl,supabaseKey)
-//console.log(supabase)
+console.log(supabase)
 export default function useSupabase(){
     return{supabase}
 }
